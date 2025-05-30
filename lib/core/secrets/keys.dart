c6bb1200be99de6d43468fa92f4377fb468a5690
Gustavo Spengler <dev.gustavospengler@gmail.com>
2025-05-30 00:21:07 -0300

    Auth corrigida

--- lib/core/secrets/keys.dart
+++ lib/core/secrets/keys.dart
@@ -3,7 +3,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class Constants {
   // Replace with your Supabase URL and anon key
   static const String supabaseUrl = 'https://dpmprspbcycelsjqvpmc.supabase.co';
-  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwbXByc3BiY3ljZWxzanF2cG1jIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0ODU3MjY5MiwiZXhwIjoyMDY0MTQ4NjkyfQ.bM4B0QEVR0Ou8OgDDTEMh5_fQC_rorH1C3Us55mAFkk';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwbXByc3BiY3ljZWxzanF2cG1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NzI2OTIsImV4cCI6MjA2NDE0ODY5Mn0.bgedJ1zg6ulhHNJ_YPuRboMq6R13I7CeW66ebdtePbI';
 }
 
 // Supabase client instance for easy access throughout the app
